7ea30423e9a465b8dd25f9d7ff60bac9f7677e38
Yonas <yonas.ghebremedhin@gmail.com>
2024-05-29 21:53:51 +0200

    style

--- wp-content/themes/mytheme/menu.php
+++ wp-content/themes/mytheme/menu.php
@@ -6,14 +6,12 @@ register_nav_menus( array(
 ) );
 
 function cart_icon_shortcode() {
-    // Check if WooCommerce is active
     if (class_exists('WooCommerce')) {
-        // Get the cart URL dynamically
         $cart_url = wc_get_cart_url();
-        // Return the cart icon with the dynamically generated URL
+        
         return '<a href="' . esc_url($cart_url) . '" class="cart-icon"><img src="/wp-content/themes/mytheme/resources/images/Cart.png" alt="Cart"></a>';
     } else {
-        return ''; // Return empty string if WooCommerce is not active
+        return ''; 
     }
 }
 add_shortcode('cart_icon', 'cart_icon_shortcode');
